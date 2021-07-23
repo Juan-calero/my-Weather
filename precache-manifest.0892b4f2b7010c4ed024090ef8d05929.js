f25e9fde4461023943a1916589727d8cd5fe19b2
Juan-calero <hello.juancalero@gmail.com>
2021-07-23 19:17:43 +0100

    Updates

---
+++ precache-manifest.0892b4f2b7010c4ed024090ef8d05929.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "396fbb77591be2c97f14",
+    "url": "/my-Weather/static/css/main.585ae040.chunk.css"
+  },
+  {
+    "revision": "396fbb77591be2c97f14",
+    "url": "/my-Weather/static/js/main.e14494bd.chunk.js"
+  },
+  {
+    "revision": "acda3639cdaa00547653",
+    "url": "/my-Weather/static/js/runtime~main.86d20c47.js"
+  },
+  {
+    "revision": "aabe2419c77f1b1d58d1",
+    "url": "/my-Weather/static/js/2.62521ab2.chunk.js"
+  },
+  {
+    "revision": "62daee26118da9f771fc",
+    "url": "/my-Weather/static/css/3.2e572fea.chunk.css"
+  },
+  {
+    "revision": "62daee26118da9f771fc",
+    "url": "/my-Weather/static/js/3.ae7f0e24.chunk.js"
+  },
+  {
+    "revision": "76e69e69db88798cd4d0",
+    "url": "/my-Weather/static/js/4.f0cec00b.chunk.js"
+  },
+  {
+    "revision": "6180025b4ca2b6c724b0",
+    "url": "/my-Weather/static/js/5.930737e4.chunk.js"
+  },
+  {
+    "revision": "38b6229a0391bb789125223a809308e6",
+    "url": "/my-Weather/static/media/sValK.38b6229a.webp"
+  },
+  {
+    "revision": "a68f0f7c02d4910aaf753172cfd0134d",
+    "url": "/my-Weather/index.html"
+  }
+];
