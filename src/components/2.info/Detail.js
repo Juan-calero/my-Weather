e7c92fa36d960192036d4ed027006d995ef08e10
Juan-calero <hello.juancalero@gmail.com>
2021-07-23 19:42:23 +0100

    Final touches Desktop Css

--- src/components/2.info/Detail.js
+++ src/components/2.info/Detail.js
@@ -1,20 +1,22 @@
-import React, { useContext } from "react"
-import { detail } from "./details.module.scss"
+import React, { Suspense, useContext } from "react"
+import { detail, noContent } from "./details.module.scss"
 import { MyContext } from "../utils/utils"
-import DetailCard from "./DetailCard"
-import DetailContent from "./DetailContent"
+const DetailCard = React.lazy(() => import("./DetailCard"))
+const DetailContent = React.lazy(() => import("./DetailContent"))
 
 function Detail() {
   const { bigCard } = useContext(MyContext)
   return (
     <>
-      {bigCard.index === undefined ? (
-        <section className={detail}></section>
-      ) : (
+      {bigCard.index !== undefined ? (
         <section className={detail}>
-          <DetailCard />
-          <DetailContent />
+          <Suspense fallback={<div></div>}>
+            <DetailCard />
+            <DetailContent />
+          </Suspense>
         </section>
+      ) : (
+        <section className={noContent} />
       )}
     </>
   )
